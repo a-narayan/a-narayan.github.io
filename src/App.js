483433194c415ac5143272666cf604e213026c53
Uday D <udaydayalbagh@gmail.com>
2024-09-06 16:05:16 -0400

    Replaced BrowserRouter with HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { AdminAddNews, AdminDashboard, AdminHome, AdminNews, AdminNewsDetails, AdminOpportunities, AdminPublications, AdminResearch, AdminResources, AdminTeam, Home, Login, News, NewsDetails, Opportunities, Publications, Research, Resources, StudentDashboard, Team, TeamMemberDetails } from './pages';
 import { DAppBar, Footer, MessageDialog } from './components';
 import ScrollToTop from './utils/scroll-to-top';
